971cbb0ca7c0ef304ee0bfbfa4b982f50240d745
Guido Schmechel <guido@ayacoo.de>
2024-02-18 14:40:26 +0100

    [TASK] Update tests

--- Tests/Unit/Rendering/SoundcloudRendererTest.php
+++ Tests/Unit/Rendering/SoundcloudRendererTest.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Ayacoo\AyacooSoundcloud\Tests\Unit\Rendering;
 
+use Ayacoo\AyacooSoundcloud\Event\ModifySoundcloudOutputEvent;
 use Ayacoo\AyacooSoundcloud\Helper\SoundcloudHelper;
 use Ayacoo\AyacooSoundcloud\Rendering\SoundcloudRenderer;
 use Psr\EventDispatcher\EventDispatcherInterface;
+use TYPO3\CMS\Core\EventDispatcher\EventDispatcher;
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Resource\Rendering\FileRendererInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
 final class SoundcloudRendererTest extends UnitTestCase
@@ -25,12 +29,12 @@ final class SoundcloudRendererTest extends UnitTestCase
         $eventDispatcherMock = $this->getMockBuilder(EventDispatcherInterface::class)->getMock();
         //$eventDispatcherMock->expects(self::atLeastOnce())->method('dispatch')->with(self::anything())->willReturnArgument(0);
 
-        $configurationManager = $this->getMockBuilder(ConfigurationManager::class)
+        $configurationManagerMock = $this->getMockBuilder(ConfigurationManager::class)
             ->onlyMethods(['getConfiguration'])
             ->disableOriginalConstructor()
             ->getMock();
 
-        $this->subject = new SoundcloudRenderer($eventDispatcherMock, $configurationManager);
+        $this->subject = new SoundcloudRenderer($eventDispatcherMock, $configurationManagerMock);
     }
 
     /**
@@ -69,11 +73,149 @@ final class SoundcloudRendererTest extends UnitTestCase
         self::assertFalse($result);
     }
 
-    private function buildReflectionForProtectedFunction(string $methodName, array $params)
+    /**
+     * @test
+     * @dataProvider getPrivacySettingWithExistingConfigReturnsBooleanDataProvider
+     * @return void
+     */
+    public function getPrivacySettingWithExistingConfigReturnsBoolean(array $pluginConfig, bool $expected)
     {
-        $reflectionCalendar = new \ReflectionClass($this->subject);
+        $eventDispatcherMock = $this->getMockBuilder(EventDispatcherInterface::class)->getMock();
+
+        $configurationManagerMock = $this->getMockBuilder(ConfigurationManager::class)
+            ->onlyMethods(['getConfiguration'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $configurationManagerMock
+            ->expects(self::atLeastOnce())
+            ->method('getConfiguration')
+            ->with(ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT)
+            ->willReturn($pluginConfig);
+
+        $subject = new SoundcloudRenderer($eventDispatcherMock, $configurationManagerMock);
+
+        $params = [];
+        $methodName = 'getPrivacySetting';
+        $result = $this->buildReflectionForProtectedFunction($methodName, $params, $subject);
+
+        self::assertEquals($expected, $result);
+    }
+
+    public static function getPrivacySettingWithExistingConfigReturnsBooleanDataProvider(): array
+    {
+        return [
+            'Privacy setting true' => [
+                [
+                    'plugin.' => [
+                        'tx_ayacoosoundcloud.' => [
+                            'settings.' => [
+                                'privacy' => true
+                            ]
+                        ]
+                    ]
+                ],
+                true
+            ],
+            'Privacy setting false' => [
+                [
+                    'plugin.' => [
+                        'tx_ayacoosoundcloud.' => [
+                            'settings.' => [
+                                'privacy' => false
+                            ]
+                        ]
+                    ]
+                ],
+                false
+            ],
+            'Privacy setting non-existing' => [
+                [],
+                false
+            ]
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function renderReturnsSoundcloudHtml(): void
+    {
+        $iframe = '<iframe src="https://www.soundcloud.com" />';
+
+        $fileResourceMock = $this->createMock(File::class);
+        $fileResourceMock->expects(self::any())->method('getMimeType')->willReturn('audio/soundcloud');
+        $fileResourceMock->expects(self::any())->method('getExtension')->willReturn('soundcloud');
+        $fileResourceMock->expects(self::any())->method('getProperty')->with('soundcloud_html')->willReturn($iframe);
+
+        $event = new ModifySoundcloudOutputEvent($iframe);
+
+        $eventDispatcherMock = $this->getMockBuilder(EventDispatcherInterface::class)->getMock();
+        $eventDispatcherMock->expects(self::once())->method('dispatch')->with(self::anything())->willReturn($event);
+
+        $configurationManagerMock = $this->getMockBuilder(ConfigurationManager::class)
+            ->onlyMethods(['getConfiguration'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $subject = new SoundcloudRenderer($eventDispatcherMock, $configurationManagerMock);
+
+        $expected = $iframe;
+
+        $result = $subject->render($fileResourceMock, 100, 100);
+        self::assertSame($expected, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function renderWithPrivacyTrueReturnsModifiedSoundcloudHtml(): void
+    {
+        $iframe = '<iframe src="https://www.soundcloud.com" />';
+        $expected = '<iframe data-name="iframe-soundcloud" data-src="https://www.soundcloud.com" />';
+
+        $fileResourceMock = $this->createMock(File::class);
+        $fileResourceMock->expects(self::any())->method('getMimeType')->willReturn('audio/soundcloud');
+        $fileResourceMock->expects(self::any())->method('getExtension')->willReturn('soundcloud');
+        $fileResourceMock->expects(self::any())->method('getProperty')->with('soundcloud_html')->willReturn($iframe);
+
+        $event = new ModifySoundcloudOutputEvent($expected);
+        $eventDispatcherMock = $this->createMock(EventDispatcherInterface::class);
+        $eventDispatcherMock->expects(self::once())->method('dispatch')->with($event)->willReturn($event);
+
+        $configurationManagerMock = $this->getMockBuilder(ConfigurationManager::class)
+            ->onlyMethods(['getConfiguration'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $pluginConfig = [
+            'plugin.' => [
+                'tx_ayacoosoundcloud.' => [
+                    'settings.' => [
+                        'privacy' => true
+                    ]
+                ]
+            ]
+        ];
+
+        $configurationManagerMock
+            ->expects(self::atLeastOnce())
+            ->method('getConfiguration')
+            ->with(ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT)
+            ->willReturn($pluginConfig);
+
+        $subject = new SoundcloudRenderer($eventDispatcherMock, $configurationManagerMock);
+
+        $result = $subject->render($fileResourceMock, 100, 100);
+        self::assertSame($expected, $result);
+    }
+
+
+    private function buildReflectionForProtectedFunction(string $methodName, array $params, SoundcloudRenderer $subject)
+    {
+        $reflectionCalendar = new \ReflectionClass($subject);
         $method = $reflectionCalendar->getMethod($methodName);
         $method->setAccessible(true);
-        return $method->invokeArgs($this->subject, $params);
+        return $method->invokeArgs($subject, $params);
     }
 }
