d1d3fb620419eb4bb635bc3dee9f5870158f6f84
Guido Schmechel <guido@ayacoo.de>
2025-02-01 15:07:35 +0100

    [TASK] Enable PHP 8.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,10 +6,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Guido Schmechel',
     'author_email' => 'info@ayacoo.de',
     'state' => 'stable',
-    'version' => '2.1.2',
+    'version' => '2.1.3',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.3.99',
+            'php' => '8.1.0-8.4.99',
             'typo3' => '12.4.2-12.9.99',
         ],
         'conflicts' => [
